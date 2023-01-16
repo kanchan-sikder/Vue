09a18cd5730ff8414dff356e10087552b20b71c5
Kanchan Sikder <73687151+kanchan-sikder@users.noreply.github.com>
2023-01-16 14:59:15 +0530

    modifeiers

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 new Vue({
     el:'#main',
     data:{
-        name: 'Kanchan',
-        job: 'Student',
-        website:'https://www.google.com',
-        webtag:'<a href="https://www.google.com">Link for html to render</a>'
+        name:'',
+        age:''
+        
     },
     methods :{
-        greet: function(time){
-            return ('Good '+ time+' '+this.name);
-        }
+        
     }
 });
