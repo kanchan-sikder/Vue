e34a1dfa399c74bc1f5f950ad0483b5c64fff2c5
Kanchan Sikder <73687151+kanchan-sikder@users.noreply.github.com>
2023-01-16 22:04:18 +0530

    Conditional

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 new Vue({
     el:'#main',
     data:{
-        name: 'Kanchan',
-        job: 'Student',
-        website:'https://www.google.com',
-        webtag:'<a href="https://www.google.com">Link for html to render</a>'
+        error:false,
+        success:false
     },
     methods :{
-        greet: function(time){
-            return ('Good '+ time+' '+this.name);
-        }
+        
+    },
+    computed:{
+        
     }
 });
