e674c4999e350d9aaa45c11d9ecc5614f438da54
Kanchan Sikder <73687151+kanchan-sikder@users.noreply.github.com>
2023-01-16 17:12:27 +0530

    Computed

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 new Vue({
     el:'#main',
     data:{
-        name: 'Kanchan',
-        job: 'Student',
-        website:'https://www.google.com',
-        webtag:'<a href="https://www.google.com">Link for html to render</a>'
+        age:19,
+        a:10,
+        b:11
+        
     },
     methods :{
-        greet: function(time){
-            return ('Good '+ time+' '+this.name);
+        
+    },
+    computed:{
+        AddA : function(){
+            console.log("A");
+            return this.age+ this.a;
+            
+        },
+        AddB : function(){
+            console.log("B");
+            return this.age+this.b;
         }
     }
 });
